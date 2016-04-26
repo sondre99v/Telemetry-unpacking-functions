ea835dba63fe0c885ebc9001ee408025ea53390d
opeide <fredrikopeide@gmail.com>
2016-04-26 19:21:49 +0200

    Update TelemetryDataUnpacking.py

--- TelemetryDataUnpacking.py
+++ TelemetryDataUnpacking.py
@@ -1,7 +1,7 @@
-            {
-            
-            '625': lambda data: [
-                ('GLVBMS current', int(self.get_byte(data,0) + self.get_byte(data,1), 16)/1000)
-            ],
-            
-            }
+{
+
+'625': lambda data: [
+    ('GLVBMS current', int(self.get_byte(data,0) + self.get_byte(data,1), 16)/1000)
+],
+
+}
