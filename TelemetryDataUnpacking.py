29d5ac8d96c44c7dd5148c7d69122c53ad93fab9
opeide <fredrikopeide@gmail.com>
2016-04-26 20:08:09 +0200

    Added GLVBMS temp 4

--- TelemetryDataUnpacking.py
+++ TelemetryDataUnpacking.py
@@ -1,7 +1,12 @@
 {
 
 '625': lambda data: [
-    ('GLVBMS current', int(data, 16)/1000)
+    ('GLVBMS_current', int(data, 16)/1000)
 ],
+
+'624': lambda data: [
+    ('GLVBMS_temp_4', int(data, 16)/100)
+],
+
 
 }
