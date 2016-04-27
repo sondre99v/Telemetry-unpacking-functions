f3ddc2bc64d5df1d956c2534c90983cde2f706cc
opeide <fredrikopeide@gmail.com>
2016-04-27 15:59:04 +0200

    Added remaining GLVBMS parsing rules

--- TelemetryDataUnpacking.py
+++ TelemetryDataUnpacking.py
@@ -23,13 +23,24 @@
 ],   
 
 '624': lambda data: [
-    ('GLVBMS_temp_4', int(data, 16)/100)
+    ('GLVBMS_temp_4', int(data, 16))
 ],
 
 '625': lambda data: [
-    ('GLVBMS_current', int(data, 16)/1000)
+    ('GLVBMS_current', int(data, 16))
 ],
 
+'626': lambda data: [
+    ('Min_voltage' , int(self.get_byte(data, 0) + self.get_byte(data, 1), 16)),
+    ('Max_voltage' , int(self.get_byte(data, 2) + self.get_byte(data, 3), 16)),
+    ('Total_voltage' , int(self.get_byte(data, 4) + self.get_byte(data, 5), 16)),
+    ('Min_temperature' , int(self.get_byte(data, 6) + self.get_byte(data, 7), 16))
+],
+
+'627': lambda data: [
+    ('Max_Temperature' , int(self.get_byte(data, 0) + self.get_byte(data, 1), 16)),
+    ('Balance_settings' , int(self.get_byte(data, 2) + self.get_byte(data, 3), 16))
+],
 
 
 
