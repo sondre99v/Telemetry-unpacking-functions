a0612deb06fa6a061566391f2fd68f35c9a88fa7
opeide <fredrikopeide@gmail.com>
2016-04-28 16:35:14 +0200

    added brackets again

--- TelemetryDataUnpacking.py
+++ TelemetryDataUnpacking.py
@@ -140,7 +140,8 @@
 
 #R16 Status data
 '293': lambda data: [
-	('R16[1] status bit_{0}'.format(i) 	, 1 if 0 != int(endian_invert(data), 32) & (2**i) else 0) for i in range(32),
+	('R16[1] status bit_{0}'.format(i) 	, 1 if 0 != int(endian_invert(data), 32) & (2**i) else 0) for i in range(32)
+	]+[
     ('R16[1] accumulated status {0}'.format(i)	, 1 if 0 != int(endian_invert(data), 32) & (2**i) else 0) for i in range(32, 64)
 ],
 
