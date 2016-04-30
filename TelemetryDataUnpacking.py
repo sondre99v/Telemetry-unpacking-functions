dbfb03d6efcdb8d401db7e28d15c555c39fc1ba7
Jakob Lover <jakob.lover@ebnett.no>
2016-04-30 03:46:34 +0200

    Changed parsing rules to accomodate parsing conventions

--- TelemetryDataUnpacking.py
+++ TelemetryDataUnpacking.py
@@ -5,35 +5,35 @@
 {
 #ADC_sensor_inputs
 '400': lambda data: [
-    ('Damper position FL' , int(get_byte(data, 1) + get_byte(data, 0), 16)/10),
-    ('Damper rate FL' , int(get_byte(data, 3) + get_byte(data, 2), 16)/10),
+    ('Damper_position_FL' , int(get_byte(data, 1) + get_byte(data, 0), 16)/10),
+    ('Damper_rate_FL' , int(get_byte(data, 3) + get_byte(data, 2), 16)/10),
 ],
 
 '401': lambda data: [
-    ('Damper position FR' , int(get_byte(data, 1) + get_byte(data, 0), 16)/10),
-    ('Damper rate FR' , int(get_byte(data, 3) + get_byte(data, 2), 16)/10),
+    ('Damper_position_FR' , int(get_byte(data, 1) + get_byte(data, 0), 16)/10),
+    ('Damper_rate_FR' , int(get_byte(data, 3) + get_byte(data, 2), 16)/10),
 ],
 
 '402': lambda data: [
-    ('Damper position RL' , int(get_byte(data, 1) + get_byte(data, 0), 16)/10),
-    ('Damper rate RL' , int(get_byte(data, 3) + get_byte(data, 2), 16)/10),
+    ('Damper_position_RL' , int(get_byte(data, 1) + get_byte(data, 0), 16)/10),
+    ('Damper_rate_RL' , int(get_byte(data, 3) + get_byte(data, 2), 16)/10),
 ],
 
 '403': lambda data: [
-    ('Damper position RR' , int(get_byte(data, 1) + get_byte(data, 0), 16)/10),
-    ('Damper rate RR' , int(get_byte(data, 3) + get_byte(data, 2), 16)/10),
+    ('Damper_position_RR' , int(get_byte(data, 1) + get_byte(data, 0), 16)/10),
+    ('Damper_rate_RR' , int(get_byte(data, 3) + get_byte(data, 2), 16)/10),
 ],
 
 '410': lambda data: [
-    ('Steering position_degrees' , int((get_byte(data, 1) + get_byte(data, 0), 16)*360)/4097)
+    ('Steering_position_degrees' , int((get_byte(data, 1) + get_byte(data, 0), 16)*360)/4097)
 ],
 
 '411': lambda data: [
-    ('TPS left' , int(get_byte(data, 1) + get_byte(data, 0), 16)/10)
+    ('TPS_left' , int(get_byte(data, 1) + get_byte(data, 0), 16)/10)
 ],
 
 '412': lambda data: [
-    ('TPS right' , int(get_byte(data, 1) + get_byte(data, 0), 16)/10)
+    ('TPS_right' , int(get_byte(data, 1) + get_byte(data, 0), 16)/10)
 ],
 
 '413': lambda data: [
@@ -41,26 +41,26 @@
 ],
 
 '420': lambda data: [
-    ('Temperature gear FL' , int(get_byte(data, 1) + get_byte(data, 0), 16)/10),
-    ('Temperature gear FR' , int(get_byte(data, 3) + get_byte(data, 2), 16)/10)
+    ('Temperature_gear_FL' , int(get_byte(data, 1) + get_byte(data, 0), 16)/10),
+    ('Temperature_gear_FR' , int(get_byte(data, 3) + get_byte(data, 2), 16)/10)
 ],
 
 '421': lambda data: [
-    ('Temperature gear RL' , int(get_byte(data, 1) + get_byte(data, 0), 16)/10),
-    ('Temperature gear RR' , int(get_byte(data, 3) + get_byte(data, 2), 16)/10)
+    ('Temperature_gear_RL' , int(get_byte(data, 1) + get_byte(data, 0), 16)/10),
+    ('Temperature_gear_RR' , int(get_byte(data, 3) + get_byte(data, 2), 16)/10)
 ],
 
 '422': lambda data: [
-    ('Temperature coolant left' , int(get_byte(data, 1) + get_byte(data, 0), 16)/10)
+    ('Temperature_coolant_left' , int(get_byte(data, 1) + get_byte(data, 0), 16)/10)
 ],
 
 '423': lambda data: [
-    ('Temperature coolant right' , int(get_byte(data, 1) + get_byte(data, 0), 16)/10)
+    ('Temperature_coolant_right' , int(get_byte(data, 1) + get_byte(data, 0), 16)/10)
 ],
 
 '421': lambda data: [
-    ('Brake pressure left' , int(get_byte(data, 1) + get_byte(data, 0), 16)/10),
-    ('Brake pressure right' , int(get_byte(data, 3) + get_byte(data, 2), 16)/10)
+    ('Brake_pressure_left' , int(get_byte(data, 1) + get_byte(data, 0), 16)/10),
+    ('Brake_pressure_right' , int(get_byte(data, 3) + get_byte(data, 2), 16)/10)
 ],
 
 #GLVBMS
@@ -100,128 +100,128 @@
 ],
 
 '627': lambda data: [
-    ('Max_Temperature' , int(get_byte(data, 0) + get_byte(data, 1), 16)),
+    ('Max_temperature' , int(get_byte(data, 0) + get_byte(data, 1), 16)),
     ('Balance_settings' , int(get_byte(data, 2) + get_byte(data, 3), 16))
 ],
 
 #R16 mechanical data 1
 '290': lambda data: [
-    ('R16[1] current speed' 	, int(get_byte(data, 0) + get_byte(data, 1), 16) / 100),
-    ('R16[1] speed setpoint' 	, int(get_byte(data, 2) + get_byte(data, 3), 16) / 100),
-    ('R16[1] torque setpoint' 	, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100)
+    ('R16[1]_current_speed' 	, int(get_byte(data, 0) + get_byte(data, 1), 16) / 100),
+    ('R16[1]_speed_setpoint' 	, int(get_byte(data, 2) + get_byte(data, 3), 16) / 100),
+    ('R16[1]_torque_setpoint' 	, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100)
 ],
 
 #R16 mechanical data 2
 '294': lambda data: [
-    ('R16[2] current speed' 	, int(get_byte(data, 0) + get_byte(data, 1), 16) / 100),
-    ('R16[2] speed setpoint' 	, int(get_byte(data, 2) + get_byte(data, 3), 16) / 100),
-    ('R16[2] torque setpoint' 	, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100)
+    ('R16[2]_current_speed' 	, int(get_byte(data, 0) + get_byte(data, 1), 16) / 100),
+    ('R16[2]_speed_setpoint' 	, int(get_byte(data, 2) + get_byte(data, 3), 16) / 100),
+    ('R16[2]_torque_setpoint' 	, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100)
 ],
 
 #R16 mechanical data 3
 '298': lambda data: [
-    ('R16[3] current speed' 	, int(get_byte(data, 0) + get_byte(data, 1), 16) / 100),
-    ('R16[3] speed setpoint' 	, int(get_byte(data, 2) + get_byte(data, 3), 16) / 100),
-    ('R16[3] torque setpoint' 	, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100)
+    ('R16[3]_current_speed' 	, int(get_byte(data, 0) + get_byte(data, 1), 16) / 100),
+    ('R16[3]_speed_setpoint' 	, int(get_byte(data, 2) + get_byte(data, 3), 16) / 100),
+    ('R16[3]_torque_setpoint' 	, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100)
 ],
 
 #R16 mechanical data 4
 '29C': lambda data: [
-    ('R16[4] current speed' 	, int(get_byte(data, 0) + get_byte(data, 1), 16) / 100),
-    ('R16[4] speed setpoint' 	, int(get_byte(data, 2) + get_byte(data, 3), 16) / 100),
-    ('R16[4] torque setpoint' 	, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100)
+    ('R16[4]_current_speed' 	, int(get_byte(data, 0) + get_byte(data, 1), 16) / 100),
+    ('R16[4]_speed_setpoint' 	, int(get_byte(data, 2) + get_byte(data, 3), 16) / 100),
+    ('R16[4]_torque_setpoint' 	, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100)
 ],
 
 #R16 temp data
 '291': lambda data: [
-    ('R16[1] temp 1' 			, int(get_byte(data, 0) + get_byte(data, 1), 16) / 100),
-    ('R16[1] temp 2' 			, int(get_byte(data, 2) + get_byte(data, 3), 16) / 100),
-    ('R16[1] temp 3' 			, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100),
-    ('R16[1] Motor temp' 				, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100)
+    ('R16[1]_temp_1' 			, int(get_byte(data, 0) + get_byte(data, 1), 16) / 100),
+    ('R16[1]_temp_2' 			, int(get_byte(data, 2) + get_byte(data, 3), 16) / 100),
+    ('R16[1]_temp_3' 			, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100),
+    ('R16[1]_motor_temp' 				, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100)
 ],
 	
 #R16 temp data		
 '295': lambda data: [		
-    ('R16[2] temp 1' 			, int(get_byte(data, 0) + get_byte(data, 1), 16) / 100),
-    ('R16[2] temp 2' 			, int(get_byte(data, 2) + get_byte(data, 3), 16) / 100),
-    ('R16[2] temp 3' 			, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100),
-    ('R16[2] Motor temp' 				, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100)
+    ('R16[2]_temp_1' 			, int(get_byte(data, 0) + get_byte(data, 1), 16) / 100),
+    ('R16[2]_temp_2' 			, int(get_byte(data, 2) + get_byte(data, 3), 16) / 100),
+    ('R16[2]_temp_3' 			, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100),
+    ('R16[2]_motor_temp' 				, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100)
 ],
 	
 #R16 temp data		
 '299': lambda data: [		
-    ('R16[3] temp 1' 			, int(get_byte(data, 0) + get_byte(data, 1), 16) / 100),
-    ('R16[3] temp 2' 			, int(get_byte(data, 2) + get_byte(data, 3), 16) / 100),
-    ('R16[3] temp 3' 			, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100),
-    ('R16[3] Motor temp' 				, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100)
+    ('R16[3]_temp_1' 			, int(get_byte(data, 0) + get_byte(data, 1), 16) / 100),
+    ('R16[3]_temp_2' 			, int(get_byte(data, 2) + get_byte(data, 3), 16) / 100),
+    ('R16[3]_temp_3' 			, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100),
+    ('R16[3]_motor_temp' 				, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100)
 ],
 
 #R16 temp data
 '29D': lambda data: [
-    ('R16[4] temp 1' 			, int(get_byte(data, 0) + get_byte(data, 1), 16) / 100),
-    ('R16[4] temp 2' 			, int(get_byte(data, 2) + get_byte(data, 3), 16) / 100),
-    ('R16[4] temp 3' 			, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100),
-    ('R16[4] Motor temp' 				, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100)
+    ('R16[4]_temp_1' 			, int(get_byte(data, 0) + get_byte(data, 1), 16) / 100),
+    ('R16[4]_temp_2' 			, int(get_byte(data, 2) + get_byte(data, 3), 16) / 100),
+    ('R16[4]_temp_3' 			, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100),
+    ('R16[4]_motor_temp' 				, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100)
 ],
 
 #R16 Electrical data
 '292': lambda data: [
-    ('R16[1] torque current' 		, int(get_byte(data, 0) + get_byte(data, 1), 16) / 100),
-    ('R16[1] direct current' 		, int(get_byte(data, 2) + get_byte(data, 3), 16) / 100),
-    ('R16[1] DC current' 		, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100),
-    ('R16[1] DC voltage' 		, int(get_byte(data, 6) + get_byte(data, 7), 16) / 100)
+    ('R16[1]_torque_current' 		, int(get_byte(data, 0) + get_byte(data, 1), 16) / 100),
+    ('R16[1]_direct_current' 		, int(get_byte(data, 2) + get_byte(data, 3), 16) / 100),
+    ('R16[1]_DC_current' 		, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100),
+    ('R16[1]_DC_voltage' 		, int(get_byte(data, 6) + get_byte(data, 7), 16) / 100)
 ],
 
 #R16 Electrical data
 '296': lambda data: [
-    ('R16[2] torque current' 	, int(get_byte(data, 0) + get_byte(data, 1), 16) / 100),
-    ('R16[2] direct current' 	, int(get_byte(data, 2) + get_byte(data, 3), 16) / 100),
-    ('R16[2] DC current' 		, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100),
-    ('R16[2] DC voltage' 		, int(get_byte(data, 6) + get_byte(data, 7), 16) / 100)
+    ('R16[2]_torque_current' 	, int(get_byte(data, 0) + get_byte(data, 1), 16) / 100),
+    ('R16[2]_direct_current' 	, int(get_byte(data, 2) + get_byte(data, 3), 16) / 100),
+    ('R16[2]_DC_current' 		, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100),
+    ('R16[2]_DC_voltage' 		, int(get_byte(data, 6) + get_byte(data, 7), 16) / 100)
 ],
 
 #R16 Electrical data
 '29A': lambda data: [
-    ('R16[3] torque current' 	, int(get_byte(data, 0) + get_byte(data, 1), 16) / 100),
-    ('R16[3] direct current' 	, int(get_byte(data, 2) + get_byte(data, 3), 16) / 100),
-    ('R16[3] DC current' 		, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100),
-    ('R16[3] DC voltage' 		, int(get_byte(data, 6) + get_byte(data, 7), 16) / 100)
+    ('R16[3]_torque_current' 	, int(get_byte(data, 0) + get_byte(data, 1), 16) / 100),
+    ('R16[3]_direct_current' 	, int(get_byte(data, 2) + get_byte(data, 3), 16) / 100),
+    ('R16[3]_DC_current' 		, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100),
+    ('R16[3]_DC_voltage' 		, int(get_byte(data, 6) + get_byte(data, 7), 16) / 100)
 ],
 
 #R16 Electrical data
 '29E': lambda data: [
-    ('R16[4] torque current' 	, int(get_byte(data, 0) + get_byte(data, 1), 16) / 100),
-    ('R16[4] direct current' 	, int(get_byte(data, 2) + get_byte(data, 3), 16) / 100),
-    ('R16[4] DC current' 		, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100),
-    ('R16[4] DC voltage' 		, int(get_byte(data, 6) + get_byte(data, 7), 16) / 100)
+    ('R16[4]_torque_current' 	, int(get_byte(data, 0) + get_byte(data, 1), 16) / 100),
+    ('R16[4]_direct_current' 	, int(get_byte(data, 2) + get_byte(data, 3), 16) / 100),
+    ('R16[4]_DC_current' 		, int(get_byte(data, 4) + get_byte(data, 5), 16) / 100),
+    ('R16[4]_DC_voltage' 		, int(get_byte(data, 6) + get_byte(data, 7), 16) / 100)
 ],
 
 #R16 Status data
 '293': lambda data: [
-	('R16[1] status bit_{0}'.format(i) 	, 1 if 0 != int(endian_invert(data), 32) & (2**i) else 0) for i in range(32)
+	('R16[1]_status_bit_{0}'.format(i) 	, 1 if 0 != int(endian_invert(data), 32) & (2**i) else 0) for i in range(32)
 	]+[
-    ('R16[1] accumulated status {0}'.format(i)	, 1 if 0 != int(endian_invert(data), 32) & (2**i) else 0) for i in range(32, 64)
+    ('R16[1]_accumulated_status_{0}'.format(i)	, 1 if 0 != int(endian_invert(data), 32) & (2**i) else 0) for i in range(32, 64)
 ],
 
 #R16 Status data
 '297': lambda data: [
-	('R16[1] status bit_{0}'.format(i) 	, 1 if 0 != int(endian_invert(data), 32) & (2**i) else 0) for i in range(32)
+	('R16[2]_status_bit_{0}'.format(i) 	, 1 if 0 != int(endian_invert(data), 32) & (2**i) else 0) for i in range(32)
     ]+[
-    ('R16[1] accumulated status {0}'.format(i)	, 1 if 0 != int(endian_invert(data), 32) & (2**i) else 0) for i in range(32, 64)
+    ('R16[2]_accumulated_status_{0}'.format(i)	, 1 if 0 != int(endian_invert(data), 32) & (2**i) else 0) for i in range(32, 64)
 ],
 
 #R16 Status data
 '29B': lambda data: [
-	('R16[1] status bit_{0}'.format(i) 	, 1 if 0 != int(endian_invert(data), 32) & (2**i) else 0) for i in range(32)
+	('R16[3]_status_bit_{0}'.format(i) 	, 1 if 0 != int(endian_invert(data), 32) & (2**i) else 0) for i in range(32)
     ]+[
-    ('R16[1] accumulated status {0}'.format(i)	, 1 if 0 != int(endian_invert(data), 32) & (2**i) else 0) for i in range(32, 64)
+    ('R16[3]_accumulated_status_{0}'.format(i)	, 1 if 0 != int(endian_invert(data), 32) & (2**i) else 0) for i in range(32, 64)
 ],
 
 #R16 Status data
 '29F': lambda data: [
-	('R16[1] status bit_{0}'.format(i) 	, 1 if 0 != int(endian_invert(data), 32) & (2**i) else 0) for i in range(32)
+	('R16[4]_status_bit_{0}'.format(i) 	, 1 if 0 != int(endian_invert(data), 32) & (2**i) else 0) for i in range(32)
     ]+[
-    ('R16[1] accumulated status {0}'.format(i)	, 1 if 0 != int(endian_invert(data), 32) & (2**i) else 0) for i in range(32, 64)
+    ('R16[4]_accumulated_status_{0}'.format(i)	, 1 if 0 != int(endian_invert(data), 32) & (2**i) else 0) for i in range(32, 64)
 ],
 
 }
